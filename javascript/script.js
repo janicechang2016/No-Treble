0670105a8a0f74b54614816f0e73dc5c0624965e
Janice Chang <janice.z.chang@gmail.com>
2017-10-02 18:50:47 -0400

    added firebase link and js

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+//Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBQUeN0tOnqjLjpR3M1W15VAx1XZd8sq8U",
+    authDomain: "no-treble.firebaseapp.com",
+    databaseURL: "https://no-treble.firebaseio.com",
+    projectId: "no-treble",
+    storageBucket: "no-treble.appspot.com",
+    messagingSenderId: "107363991076"
+  };
+  firebase.initializeApp(config);
