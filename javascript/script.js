51d6adb13e40462616f2309fd12693abd90c28ab
David Sosa <sosa413131@yahoo.com>
2017-10-04 19:18:40 -0400

    hmtl connected to script.js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+console.log("javascript working!")
+
 //Initialize Firebase
   var config = {
     apiKey: "AIzaSyBQUeN0tOnqjLjpR3M1W15VAx1XZd8sq8U",
@@ -8,3 +10,70 @@
     messagingSenderId: "107363991076"
   };
   firebase.initializeApp(config);
+
+  database = firebase.database();
+
+
+var query = "brown eyed girl";
+
+for (i = 0; i < query.length, i++);
+
+ 
+
+var queryConverted = query.replace(" ", "%20");
+
+console.log(queryConverted);
+
+queryURL = "https://rutgers-genius-proxy.herokuapp.com/search?q=" + queryConverted;
+
+console.log(queryURL);
+
+// queryURL = "https://api.musixmatch.com/ws/1.1/"
+
+// // <script type="text/javascript" src="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuOP">
+
+
+
+// apiKey = "6824e3020fba9ce389b04011d47887d4"
+
+
+// curl --get --include 'https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/artist.get?artist_id=1039' \
+//   -H 'X-Mashape-Key: tymAkvmzc3msh93vtoFMlA6lOpxLp1FWvYWjsn4RZdPKYlKIqJ' \
+//   -H 'Accept: application/json'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   // authentication for genius API
+// var clientId = "AZgkZko3TFFzayuBtAZWzsNzptuOzSVSUVSEAkbfqbdN5qP7jOPJIyzpcVeW4BKX";
+
+
+
+// //    // authentication for genius API
+// var clientSecret  = "nYYz8dcvxiLrnMr0Of4jLkRmwuEsJgTUoVwfa6DB5bfMaRJbT9QYdwPdpusW9H_wx6HcE5bOV-56hplJxR851Q"
+
+
+// // // event handler for button
+// // 	$(document).on("click","#", function(){
+
+
+// // 	var query = $(this)
+
+// // 	// string with API key and query gets concatenated to create URL;
+// 	var queryURL = "https://api.genius.com/oauth/authorize" + query + "? ";
+
+
+
+
+	
